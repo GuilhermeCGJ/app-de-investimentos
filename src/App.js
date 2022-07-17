6cd8068076eea99a51d77eb2327d085da035c585
Guilherme Carvalho <guil_car@hotmail.com>
2022-07-17 17:46:50 -0300

    add: Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
 import ExpProvider from './context/ExpProvider';
+import Transactions from './pages/Transactions';
+import Login from './pages/Login';
 
 function App() {
   return (
     <ExpProvider>
       <div className="App">
-        Teste
+        <Routes>
+          <Route exact path="/" element={ <Login /> } />
+          <Route exact path="/home" element={ <Home /> } />
+          <Route exact path="/transactions" element={ <Transactions /> } />
+          <Route path="*" element={ <Login /> } />
+        </Routes>
       </div>
     </ExpProvider>
   );
