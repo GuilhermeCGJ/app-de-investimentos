3755e305c3ad45865be0dfa0f513427a7aa28724
Guilherme Carvalho <guil_car@hotmail.com>
2022-07-17 18:28:22 -0300

    adc: Avaiable Stocks Component

---
+++ src/components/AvailableStocks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AvailableStocks () {
+
+  return (
+    <div>
+    </div>
+  )
+  };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useEffect, useContext } from 'react';
+import AvailableStocks from '../components/AvailableStocks';
+import ExpContext from '../context/ExpContext';
 
 export default function Home () {
 
+  const {
+    getData,
+    } = useContext(ExpContext);
+
+
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  
+
   return (
     <div id='Home'>
+      <AvailableStocks />
     </div>
   )
 }
