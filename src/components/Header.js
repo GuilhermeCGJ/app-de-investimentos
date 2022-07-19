12a05c834216e180182f83a4e75467f7f5505994
Guilherme Carvalho <guil_car@hotmail.com>
2022-07-19 19:13:27 -0300

    adc: Return Button on Transactions Page

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MyStocksArea () {
+
+  return (
+    <div id='header'>
+      <div className='header'>
+      </div>
+    </div>
+  )
+}
--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Balance from '../components/Balance'
 import DepositAndWithdrawal from '../components/DepositAndWithdrawal'
+import { useNavigate } from 'react-router-dom'
 import './Transactions.css'
 
 export default function Transactions () {
+  const navigate = useNavigate ();
 
+  const handleClick = () => {
+    navigate("../home", { replace: true })
+  }; 
+  
   return (
     <div id='transactions'>
-      <button>
+      <button type="button" onClick= { handleClick }>
         Voltar
       </button>
       <Balance />
